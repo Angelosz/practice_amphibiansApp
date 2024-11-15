2648163a6e44924a10721cb9eaa43df214cc00a8
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-15 00:29:01 +0000

    Implements HomeScreen so it takes into account HomeScreenUiState state

--- app/src/main/java/angelosz/practice_amphibiansapp/ui/Homescreen.kt
+++ app/src/main/java/angelosz/practice_amphibiansapp/ui/Homescreen.kt
@@ -1,21 +1,69 @@
 package angelosz.practice_amphibiansapp.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import angelosz.practice_amphibiansapp.R
 import angelosz.practice_amphibiansapp.model.Amphibian
 import angelosz.practice_amphibiansapp.ui.theme.Practice_amphibiansAppTheme
+import angelosz.practice_amphibiansapp.view_model.HomeScreenUiState
 
 @Composable
-fun HomeScreen(amphibianList: List<Amphibian>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(items = amphibianList){ amphibian ->
+fun HomeScreen(
+    uiState: HomeScreenUiState,
+    modifier: Modifier = Modifier
+) {
+    when(uiState){
+        is HomeScreenUiState.Success -> AmphibiansList(uiState.amphibians)
+        HomeScreenUiState.Error -> ErrorScreen()
+        HomeScreenUiState.Loading -> LoadingScreen()
+    }
+}
+
+@Composable
+private fun AmphibiansList(
+    amphibians: List<Amphibian>,
+) {
+    LazyColumn() {
+        items(items = amphibians) { amphibian ->
             AmphibianDataCard(amphibian, Modifier.padding(24.dp))
         }
+    }
+}
+
+@Composable
+private fun LoadingScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = stringResource(R.string.loading),
+        )
+    }
+}
+
+@Composable
+private fun ErrorScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = stringResource(R.string.failed_to_load_amphibians_data),
+        )
     }
 }
 
@@ -49,6 +97,8 @@ fun HomeScreenPreview(){
                 imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
             )
         )
-        HomeScreen(amphibianList)
+        HomeScreen(
+            uiState = HomeScreenUiState.Success(amphibianList),
+            )
     }
 }
